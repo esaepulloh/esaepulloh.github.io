6d9ffd77e0c614b9e653d269c180cf755bcb0fb2
esaepulloh <saepulloh2512@gmail.com>
2023-09-13 21:23:03 +0700

    update detail

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,13 +1,10 @@
-function sendmail()
+function sendmail(e)
 {
-    
+    e.preventDefault()
     let name= document.getElementById("name").value;
     let email= document.getElementById("email").value;
     let message= document.getElementById("comment").value;
-
-
-     let body = `From: ${name + " - " +email} <br> Message: ${message}`;
-
+    let body = '<br>Name: ' + name +"<br>Email: " +email + "<br>Message: " + message
 
     Email.send({
         SecureToken:"231f7eb7-9bf3-4a73-8b12-1dbb0444e337",
@@ -17,15 +14,26 @@ function sendmail()
         Body:body,
     }).then(
         message =>{
-           
+
             if(message=='OK'){
             alert('Your Message Successfully Delivered!');
             }
             else{
                 console.error (message);
-                alert('There is error at sending e-mail. ')
-                
+                alert('There is error at sending e-mail.') 
             }
         }
     );
 }
+
+function reset () {
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("comment").value = "";
+    }
+
+document.getElementsByTagName("form")[0].addEventListener("submit", (e) => {
+    sendmail(e);
+    reset();
+    return false;
+    })
